f608929f72cafd32bee64015cd343bd26231bc59
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-19 20:47:31 -0500

    commit tree e7c584768e91823ff427618d89b0e9bf3de016fe

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name) 
+   puts "Hello, #{name}!"
+end
+
+# greet "Naureen"
+greet("Naureen")
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+add 1, 2
+
+def halve (num1)
+    if num1.class != Integer
+        return nil
+    end
+    num1 / 2
+end
+
+halve 6
